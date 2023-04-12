a80c91097581f8f4ea4d784f1c8701d4ba09936d
PG one1 <9903767+pg-one1@user.noreply.gitee.com>
2023-04-12 19:13:50 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,4 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:PGthirteen/blog.git main:gh-pages
+git push -f git@github.com:PGthirteen/learn-typescript.git main:gh-pages
